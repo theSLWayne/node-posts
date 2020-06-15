bc1a6b46ef4c3aec975c7da25a9664cfa0235688
theSLWayne <hiranhasanka@gmail.com>
2020-06-15 23:51:52 +0530

    Retrieve posts from the database

--- routes/posts.js
+++ routes/posts.js
@@ -2,25 +2,29 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
 
-router.get('/', (req, res) => {
-    res.send("You're on posts")
+router.get('/', async(req, res) => {
+    try {
+        // This will return all posts
+        const posts = await Post.find()
+        res.json(posts)
+    } catch (err) {
+        res.json({ message: err })
+    }
 })
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     })
 
     // saving the post details to the database. This process is done as a promise.
-    post.save()
-        .exec()
-        .then(data => {
-            res.json(data)
-        })
-        .catch(err => {
-            res.json({ message: err })
-        })
+    try {
+        const savedPost = await post.save()
+        res.json(savedPost)
+    } catch (err) {
+        res.json({ message: err })
+    }
 })
 
 module.exports = router
